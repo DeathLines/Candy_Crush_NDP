747889982945cb70f6bee218f0b34a0a00e3f37c
DeathLines <deathlines11@gmail.com>
2024-12-23 15:36:12 +0300

    Jokerler için yeni bir algoritmaya geçmeden önce kayıt

--- Game_Page.cs
+++ Game_Page.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,6 +22,7 @@ namespace NDP_Proje
         private int end_of_per = 0;
         private int start_of_per = 0;
         private bool done;
+        //private bool changed = true;
         private PictureBox[] pics;
 
 
@@ -72,12 +74,24 @@ namespace NDP_Proje
             }
 
             //Seker1 ab = new Seker1(10, 5, "a");
-            //Console.WriteLine($"Seker1: x = {ab.x}, y = {ab.x}, type = {ab.type}");
+            ////Console.WriteLine($"Seker1: x = {ab.x}, y = {ab.x}, type = {ab.type}");
             //pictureBox1.BackgroundImage 
+
+            DeleteSugars();
+
 
         }
 
 
+
+        //private void check_changes()
+        //{
+        //    while (changed)
+        //    {
+        //        Console.WriteLine("Değişiklik var");
+        //        changed = DeleteSugars();
+        //    }
+        //}
 
         private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
         {
@@ -90,11 +104,11 @@ namespace NDP_Proje
         {
             if (e.Button == MouseButtons.Left)
             {
-                Console.WriteLine("Mouse Left Click");
+                //Console.WriteLine("Mouse Left Click");
                 //SugarDragDrop(sender);
                 //DoDragDrop(sender, DragDropEffects.Move);
                 PictureBox picbox = (PictureBox)sender;
-                Console.WriteLine(picbox.Name);
+                //Console.WriteLine(picbox.Name);
 
                 if (select1 == 0)
                 {
@@ -133,24 +147,28 @@ namespace NDP_Proje
                     // Yer değiştirme işlemi bitti 
 
                     DeleteSugars();
+                    //check_changes();
 
 
 
                 }
             }
-            Console.WriteLine("$$$$$MOUSE__DOWN$$$$");
+            //Console.WriteLine("$$$$$MOUSE__DOWN$$$$");
         }
 
 
-        private void DeleteSugars()
+        private bool DeleteSugars()
         {
+            Thread.Sleep(100);
+            Console.WriteLine("DeleteSugars");
+            bool basechanged = false;
             string[] jokers = { "item_dikey_roket.png", "item_yatay_roket.png", "item_bomba.png", "item_gokkusagi.png", "item_helicopter.png" };
 
             // YATAY YOK EDİŞ
             for (int i = 0; i < pics.Count() - 1; i++)  // Bütün satırları gez
             {
-                //Console.WriteLine($"AccName: {pics[i].AccessibleName}");
-                //Console.WriteLine($"########\npic[i]: {pics[i]}\npics[i].AccessibleName: {pics[i].AccessibleName}\npics[i].Tag: {pics[i].Tag}\n#########");
+                ////Console.WriteLine($"AccName: {pics[i].AccessibleName}");
+                ////Console.WriteLine($"########\npic[i]: {pics[i]}\npics[i].AccessibleName: {pics[i].AccessibleName}\npics[i].Tag: {pics[i].Tag}\n#########");
 
                 // Eğer format picboxuna geldiyse geç
                 if ((pics[i].AccessibleName == "Newline") || (pics[i].AccessibleName == "NewlineUp"))
@@ -161,7 +179,7 @@ namespace NDP_Proje
                 // Eğer son elemana geldiyse geç
                 if (i + 1 > pics.Count() - 1)
                 {
-                    Console.WriteLine("i Aşımı");
+                    //Console.WriteLine("i Aşımı");
                     continue;
                 }
 
@@ -184,13 +202,13 @@ namespace NDP_Proje
                         if (per_count >= 3)
                         {
                             // Eğer joker varsa
-                            if (jokers.Contains(pics[i + 1].Tag))
+                            if (jokers.Contains(pics[i + 1].Tag) || jokers.Contains(pics[i - per_count].Tag))
                             {
                                 switch (pics[i + 1].Tag)
                                 {
                                     case "item_dikey_roket.png":
-                                        Console.WriteLine("Dikey Roket");
-
+                                        //Console.WriteLine("Dikey Roket");
+                                        dikey_roket(i);
                                         break;
                                     case "item_yatay_roket.png":
                                         var jok_result = yatay_roket(i);
@@ -198,17 +216,44 @@ namespace NDP_Proje
                                         i = jok_result.Item2;
                                         break;
                                     case "item_bomba.png":
-                                        Console.WriteLine("Bomba");
+                                        //Console.WriteLine("Bomba");
                                         break;
                                     case "item_gokkusagi.png":
-                                        Console.WriteLine("Gökkuşağı");
+                                        //Console.WriteLine("Gökkuşağı");
                                         break;
                                     case "item_helicopter.png":
-                                        Console.WriteLine("Helicopter");
+                                        //Console.WriteLine("Helicopter");
                                         break;
 
                                     default:
                                         break;
+                                }
+                                if ((i - per_count) > 0)
+                                {
+                                    switch (pics[i - per_count].Tag)
+                                    {
+                                        case "item_dikey_roket.png":
+                                            //Console.WriteLine("Dikey Roket");
+                                            dikey_roket(i - per_count - 1);
+                                            break;
+                                        case "item_yatay_roket.png":
+                                            var jok_result = yatay_roket(i);
+                                            done = jok_result.Item1;
+                                            i = jok_result.Item2;
+                                            break;
+                                        case "item_bomba.png":
+                                            //Console.WriteLine("Bomba");
+                                            break;
+                                        case "item_gokkusagi.png":
+                                            //Console.WriteLine("Gökkuşağı");
+                                            break;
+                                        case "item_helicopter.png":
+                                            //Console.WriteLine("Helicopter");
+                                            break;
+
+                                        default:
+                                            break;
+                                    }
                                 }
                             }
 
@@ -217,6 +262,12 @@ namespace NDP_Proje
                             {
                                 for (int j = i; j > i - per_count; j--)
                                 {
+                                    //changed = true;
+                                    basechanged = true;
+                                 if ((pics[j].AccessibleName == "NewLineUp") || (pics[j].AccessibleName == "End"))
+                                        {
+                                            continue;
+                                        }
                                     // Silinen şekerlerin yerine yeni şekerler ekleniyor
                                     var result_CreateImage = CreateSugarImage();
                                     Image _Image = result_CreateImage.Item1;
@@ -229,15 +280,24 @@ namespace NDP_Proje
                             }
                         }
 
+                        //else
+                        //{
+                        //    changed = false;
+                        //}
+
                         done = true;
                     }
-                    
+                    else
+                    {
+                        //changed = false;
+                    }
+
                     //else
 
-                    Console.WriteLine($"@@pics[i].Tag: {pics[i].Tag}");
-                    Console.WriteLine($"@@pics[i+1].Tag: {pics[i + 1].Tag}");
+                    //Console.WriteLine($"@@pics[i].Tag: {pics[i].Tag}");
+                    //Console.WriteLine($"@@pics[i+1].Tag: {pics[i + 1].Tag}");
                 }
-                Console.WriteLine(per_count);
+                //Console.WriteLine(per_count);
                 per_count = 0;
             }
 
@@ -254,17 +314,17 @@ namespace NDP_Proje
                 {
                     if (j + 8 > pics.Count() - 1)
                     {
-                        Console.WriteLine("i Aşımı");
+                        //Console.WriteLine("i Aşımı");
                         continue;
                     }
-                    //Console.WriteLine($"##pics[j].TAG: {pics[j].Tag}\npics[j+8].TAG: {pics[j + 8].Tag}");
+                    ////Console.WriteLine($"##pics[j].TAG: {pics[j].Tag}\npics[j+8].TAG: {pics[j + 8].Tag}");
                     if (pics[j].Tag == pics[j + 8].Tag)
                     {
                         start_of_per = j;
                         per_count += 8;
                         while (pics[j].Tag == pics[j + 8].Tag)
                         {
-                            //Console.WriteLine($"##pics[j].TAG: {pics[j].Tag}\npics[j+8].TAG: {pics[j + 8].Tag}");
+                            ////Console.WriteLine($"##pics[j].TAG: {pics[j].Tag}\npics[j+8].TAG: {pics[j + 8].Tag}");
                             per_count += 8;
                             j += 8;
                             end_of_per = j;
@@ -274,6 +334,12 @@ namespace NDP_Proje
                         {                       //NEGATİF OLUYOR PER COUNT J DEN HEP 5 DAHA BÜYÜK ÇIKIYOR
                             for (int k = end_of_per; k >= start_of_per; k -= 8)
                             {
+                                //changed = true;
+                                basechanged = true;
+                                if ((pics[k].AccessibleName == "NewLineUp") || (pics[k].AccessibleName == "End"))
+                                {
+                                    continue;
+                                }
                                 // Silinen şekerlerin yerine yeni şekerler ekleniyor
                                 var result_CreateImage = CreateSugarImage();
                                 Image _Image = result_CreateImage.Item1;
@@ -283,16 +349,25 @@ namespace NDP_Proje
                             }
                         }
                     }
+                    //else
+                    //{
+                    //    changed = false;
+                    //}
                 }
             }
 
 
+            if (basechanged)
+            {
+                DeleteSugars();
+            }
+            return basechanged;
         }
 
-        private (bool,int) yatay_roket(int i)
+        private (bool, int) yatay_roket(int i)
         {
             int new_i = 0;
-            Console.WriteLine("Yatay Roket");
+            //Console.WriteLine("Yatay Roket");
             while ((pics[i - 1].AccessibleName != "NewLineUp") && (pics[i - 1].AccessibleName != "End"))
             {
 
@@ -302,6 +377,10 @@ namespace NDP_Proje
 
             for (int j = i; j < i + 6; j++)
             {
+                if ((pics[j].AccessibleName == "NewLineUp") || (pics[j].AccessibleName == "End"))
+                {
+                    continue;
+                }
                 var result_CreateImage = CreateSugarImage();
                 Image _Image = result_CreateImage.Item1;
                 String _Tag = result_CreateImage.Item2;
@@ -310,9 +389,33 @@ namespace NDP_Proje
                 new_i = j;
             }
 
-            return (false,new_i);
+
+            DeleteSugars();
+            return (false, new_i);
 
         }
+
+        private void dikey_roket(int i)
+        {       // pics.Count() = 48;
+            int start_of_dikey_rocket = (i % 8) + 1; // 4
+            //Console.WriteLine("Dikey Roket");
+            for (int j = (i % 8) + 1; j < pics.Count(); j += 8)
+            {
+                if ((pics[j].AccessibleName == "NewLineUp") || (pics[j].AccessibleName == "End"))
+                {
+                    continue;
+                }
+                var result_CreateImage = CreateSugarImage();
+                Image _Image = result_CreateImage.Item1;
+                String _Tag = result_CreateImage.Item2;
+                pics[j].BackgroundImage = _Image;
+                pics[j].Tag = _Tag;
+
+            }
+            DeleteSugars();
+        }
+
+
 
         private void SaveSugarPictureBox(string name, Image image, Point location, Size size, object Tag)
         {
